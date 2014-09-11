e198e3f9b92b36ec4a7af9e6b75a71f12e236f71
goodolclint <goodolclint@gmail.com>
2014-09-11 10:38:51 -0500

    Added AsyncJob Queue to PSRunspace class

--- PSAsync/PSRunspace.cs
+++ PSAsync/PSRunspace.cs
@@ -1,5 +1,8 @@
-﻿using System.Management.Automation;
+﻿using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using System.Threading;
 
 namespace PSAsync
 {
@@ -25,40 +28,69 @@ namespace PSAsync
         }
         #endregion
 
+        private Semaphore WorkLimit;
+
         private PSRunspace()
         {
             this.PoolSize = 20;
             this.IsOpen = false;
+            this.JobQueue = new ConcurrentQueue<AsyncJob>();
         }
 
         private RunspacePool pool;
 
         public bool IsOpen { get; private set; }
         public int PoolSize { get; set; }
-        public AuthenticationMechanism Authentication { get; set; }
-        public PSCredential Credential { get; set; }
 
         public void Open()
         {
             if (!this.IsOpen)
             {
                 this.IsOpen = true;
-                if (null != this.pool)
+                if (this.pool == null)
                 {
                     this.pool = RunspaceFactory.CreateRunspacePool(1, this.PoolSize);
-                    this.pool.ConnectionInfo.AuthenticationMechanism = this.Authentication;
-                    this.pool.ConnectionInfo.Credential = this.Credential;
+                    this.WorkLimit = new Semaphore(this.PoolSize, this.PoolSize);
+                    Thread t = new Thread(this.StartJobs);
+                    t.Start();
                 }
                 this.pool.Open();
             }
         }
 
+        private void StartJobs()
+        {
+            if (this.JobQueue.Count > 0)
+            {
+                this.WorkLimit.WaitOne();
+                AsyncJob data;
+                if (this.JobQueue.TryDequeue(out data))
+                {
+                    data.StartJob();
+                    data.StateChanged += data_StateChanged;
+                }
+            }
+            Thread.Sleep(250);
+            this.StartJobs();
+        }
+
+        void data_StateChanged(object sender, JobStateEventArgs e)
+        {
+            if (e.JobStateInfo.State == JobState.Completed)
+            { this.WorkLimit.Release(); }
+        }
+
         public PowerShell NewPipeline()
         {
+            this.Open();
             var pipeline = PowerShell.Create();
             pipeline.RunspacePool = this.pool;
             return pipeline;
         }
+
+        private ConcurrentQueue<AsyncJob> JobQueue;
+        public void AddJob(AsyncJob Job)
+        { this.JobQueue.Enqueue(Job); }
 
         public void Close()
         {
