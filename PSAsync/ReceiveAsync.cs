ebb48a3b44185d88183ff1e6bf24a329b08766e2
goodolclint <goodolclint@gmail.com>
2014-09-11 14:42:38 -0500

    Added Receive-Async CMDlet

--- PSAsync/ReceiveAsync.cs
+++ PSAsync/ReceiveAsync.cs
@@ -7,7 +7,30 @@ using System.Management.Automation;
 namespace PSAsync
 {
     [Cmdlet(VerbsCommunications.Receive, "Async")]
+    [CmdletBinding(DefaultParameterSetName = "ID")]
     public class ReceiveAsync : PSCmdlet
     {
+        [Parameter(ParameterSetName = "ID", Mandatory = true)]
+        public int[] Id { get; set; }
+
+        [Parameter(ParameterSetName = "Name", Mandatory = true)]
+        public string[] Name { get; set; }
+
+        [Parameter(ParameterSetName = "Default", Mandatory = true, ValueFromPipeline = true)]
+        public List<AsyncJob> InputObject { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            AsyncJob[] jobs;
+            if (this.ParameterSetName == "ID")
+            { jobs = PSRunspace.Instance.JobQueue.Where(j => this.Id.Contains(j.Id)).ToArray(); }
+            else if (this.ParameterSetName == "Name")
+            { jobs = PSRunspace.Instance.JobQueue.Where(j => this.Name.Contains(j.Name)).ToArray(); }
+            else
+            { jobs = PSRunspace.Instance.JobQueue.ToArray(); }
+
+            foreach (AsyncJob j in jobs)
+            { WriteObject(j.GetJob()); }
+        }
     }
 }
